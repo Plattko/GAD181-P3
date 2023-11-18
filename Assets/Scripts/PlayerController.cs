ce8476a22e88fb233f2f10f399b684f3999420cd
Clay M - Home <140664778+Plattko@users.noreply.github.com>
2023-11-18 18:02:06 +1100

    Updated Player Movement to Forces, Updated Camera
    Player Controller script:
    - Added new movement and jump variables.
    - Simplified player-specific variables.
    - Updated Update method to reset/update the coyote time counter if the player is/isn't grounded respectively.
    - Updated the player's horizontal movement in FixedUpdate to be calculated with a force that accelerates/decelerates the player to match their target speed.
    - Updated FixedUpdate method to calculate friction that slows the player when they are grounded and not inputting a movement direction.
    - Updated FixedUpdate method to increase the player's gravity scale when they are falling.
    - Updated IsGrounded bool to use an OverlapBox instead of an OverlapCircle, allowing the player to jump when at the edge of a platform.
    - Updated the first jump in the OnJump method to check if the coyote timer counter is above zero instead of whether the player is grounded, allowing them to jump for a brief period after dropping off a ledge.
    - Updated the double jump in the OnJump method to cancel their current vertical velocity.
    - Updated Player 2's double jump in the OnJump method to still give the player vertical momentum if there is no horizontal movement input.
    - Updated cancelling a jump in the OnJump method to set the coyote time counter to 0.
    - Updated cancelling a jump in the OnJump method to reduce the player's upward momentum using AddForce instead of setting a new velocity.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,21 +11,32 @@ public class PlayerController : MonoBehaviour
     private LayerMask groundLayer;
     public Transform otherPlayer;
 
+    // Move variables
     private float horizontalInput;
-    private float moveSpeed = 5f;
+    private float moveSpeed = 8f;
+    private float acceleration = 7f;
+    private float deceleration = 7f;
+    private float velPower = 0.9f;
+
+    private float frictionAmount = 0.25f;
+
+    // Jump variables
     private float jumpPower = 8f;
+    private float coyoteTime = 0.15f;
+    private float coyoteTimeCounter;
+    private float gravityScale = 1f;
+    private float gravityFallMultiplier = 1.9f;
+
     private bool doubleJumpUsed = false;
 
+    // Player-specific variables
     private enum PlayerType
     {
         Player1,
         Player2
     }
     [SerializeField] private PlayerType playerType;
-
-    // Player-specific variables
-    private float p1HighJumpPower = 5f;
-    private float p2LungePower = 30f;
+    [SerializeField] private float doubleJumpPower = 10f;
     
     // Start is called before the first frame update
     void Start()
@@ -41,17 +52,57 @@ public class PlayerController : MonoBehaviour
         if (IsGrounded())
         {
             doubleJumpUsed = false;
+            coyoteTimeCounter = coyoteTime;
+        }
+        else
+        {
+            coyoteTimeCounter -= Time.deltaTime;
         }
     }
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
+        //rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
+
+        // Calculate the move direction and the desired velocity
+        float targetSpeed = horizontalInput * moveSpeed;
+        // Calculate the difference between the current and desired velocity
+        float speedDiff = targetSpeed - rb.velocity.x;
+        // Change between acceleration or deceleration depending on whether the player is providing an input
+        float accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? acceleration : deceleration;
+        // Apply acceleration to the speed difference, then raise to a set power so acceleration increases with higher speeds
+        // Multiply by sign to reapply direction
+        float movement = Mathf.Pow(Mathf.Abs(speedDiff) * accelRate, velPower) * Mathf.Sign(speedDiff);
+
+        // Apply movement force to the Rigidbody on the x axis
+        rb.AddForce(movement * Vector2.right);
+
+        // Friction
+        if (IsGrounded() && Mathf.Abs(horizontalInput) < 0.01f)
+        {
+            // Choose the smallest value between the player's current velocity and the friction amount
+            float amount = Mathf.Min(Mathf.Abs(rb.velocity.x), Mathf.Abs(frictionAmount));
+            // Set movement direction
+            amount *= Mathf.Sign(rb.velocity.x);
+            // Apply friction force opposite to the player's movement direction
+            rb.AddForce(Vector2.right * -amount, ForceMode2D.Impulse);
+        }
+
+        if (rb.velocity.y < 0)
+        {
+            rb.gravityScale = gravityScale * gravityFallMultiplier;
+        }
+        else
+        {
+            rb.gravityScale = gravityScale;
+        }
     }
 
     private bool IsGrounded()
     {
-        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        //return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+
+        return Physics2D.OverlapBox(new Vector2(groundCheck.position.x, groundCheck.position.y), new Vector2(0.8f, 0.2f), 0f, groundLayer);
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -63,9 +114,9 @@ public class PlayerController : MonoBehaviour
     {
         if (context.performed)
         {
-            if (IsGrounded())
+            if (coyoteTimeCounter > 0f)
             {
-                rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+                rb.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
                 Debug.Log("Executed first jump.");
             }
             else if (!doubleJumpUsed)
@@ -73,27 +124,38 @@ public class PlayerController : MonoBehaviour
                 Debug.Log("Called second jump.");
                 doubleJumpUsed = true;
 
+                // Cancel vertical velocity
+                rb.velocity = new Vector2(rb.velocity.x, 0f);
+
+                // Double jump
                 switch (playerType)
                 {
                     case PlayerType.Player1:
-                        rb.velocity = new Vector2(rb.velocity.x, p1HighJumpPower);
-                        rb.AddForce(Vector2.up * p1HighJumpPower, ForceMode2D.Impulse);
+                        //rb.velocity = new Vector2(rb.velocity.x, p1HighJumpPower);
+                        
+                        rb.AddForce(Vector2.up * doubleJumpPower, ForceMode2D.Impulse);
                         break;
 
                     case PlayerType.Player2:
-                        Vector2 direction = new Vector2(horizontalInput, 0);
-                        //Quaternion rotation = Quaternion.identity;
-                        //if (horizontalInput < 0)
-                        //{
-                        //    rotation = Quaternion.Euler(0, 0, -180);
-                        //}
-                        //else if (horizontalInput > 0)
-                        //{
-                        //    rotation = Quaternion.Euler(0, 0, 180);
-                        //}
-                        //Vector2 forceDirection = rotation * direction;
-                        Debug.Log("Direction is: " + direction);
-                        rb.AddForce(direction * p2LungePower, ForceMode2D.Impulse);
+
+                        Vector2 direction = Vector2.right;
+                        Quaternion rotation = Quaternion.Euler(0, 0, 28);
+
+                        if (Mathf.Abs(horizontalInput) > 0.01f)
+                        {
+                            direction = new Vector2(horizontalInput, 0);
+                            rotation = Quaternion.Euler(0, 0, Mathf.Sign(horizontalInput) * 28);
+
+                            Vector2 forceDirection = rotation * direction;
+                            rb.AddForce(forceDirection * doubleJumpPower, ForceMode2D.Impulse);
+                        }
+                        else if (Mathf.Abs(horizontalInput) < 0.01f)
+                        {
+                            Vector2 forceDirection = rotation * direction;
+                            float lungeVerticalForce = forceDirection.y * doubleJumpPower;
+
+                            rb.AddForce(Vector2.up * lungeVerticalForce, ForceMode2D.Impulse);
+                        }
                         break;
                 }
                 Debug.Log("Executed second jump.");
@@ -102,7 +164,10 @@ public class PlayerController : MonoBehaviour
         
         if (context.canceled && rb.velocity.y > 0f)
         {
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+            coyoteTimeCounter = 0f;
+            
+            //rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+            rb.AddForce(Vector2.down * rb.velocity.y * 0.5f, ForceMode2D.Impulse);
         }
     }
 
